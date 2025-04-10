2a21b60a71342ef6e1f6f04a6b3f14465e09cf49
navneetbhaltilak <bhaltilaknavneet@gmail.com>
2025-04-10 22:59:59 +0530

    made some changes

--- myelif.py
+++ myelif.py
@@ -1,16 +1,18 @@
-a=int(input("Enter hour ="))
-b=int(input("Enter minute ="))
-c=int(input("Enter second ="))
-e=str(input("Enter your name ="))
-d=print("Time is ",a,":",b,":",c,)
-if(6<=a<=11 and 0<=b<=59 and 0<=c<=59) :
-  print("Good Morning Mr.",e)
-elif (17>=a>=12 and 59>=b>=0 and 59>=c>=0):
-    print("Good Afteernoon Mr.",e)
-elif(19>=a>=18 and 59>=b>=0 and 59>=c>=0):
-      print("Good Eveningn Mr.",e)
-elif (23>=a>=20 and 59>=b>=0 and 59>=c>=0) :
-      print("Good Night Mr.",e)
+import time
+c=str(input("Enter your name ="))
+
+t=time.strftime('%H,%M,%S')
+a=int(time.strftime('%H'))
+b=int(time.strftime('%M'))
+print("Time is : ",a,':',b)
+if(6<=a<=11 and 0<=b<=59 ) :
+  print("Good Morning Mr.",c)
+elif (17>=a>=12 and 59>=b>=0 ):
+    print("Good Afteernoon Mr.",c)
+elif(19>=a>=18 and 59>=b>=0 ):
+      print("Good Eveningn Mr.",c)
+elif (23>=a>=20 and 59>=b>=0) :
+      print("Good Night Mr.",c)
 
 else :
   print("invalid")
